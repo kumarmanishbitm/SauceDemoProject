f93825fd047d844c03dad4a2dcfb632c3c1b05a5
kumarmanishbitm <kumarmanishbitm@gmail.com>
2023-02-28 07:48:05 +0530

    git Second Commit after adding new test class

---
+++ src/test/java/com/sauce/testCases/TestGit.java
@@ -0,0 +1,13 @@
+package com.sauce.testCases;
+
+import org.testng.annotations.Test;
+
+public class TestGit {
+	
+	@Test
+	public void test(){
+		
+		System.out.println("This is a test class.");
+	}
+
+}
